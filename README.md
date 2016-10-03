2dd59ffbc77fdfef15331d0c86345a5c21ee59a7
Anton Grischenko <f0rmat1k@yandex-team.ru>
2016-10-03 20:26:11 +0500

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# stylelint-config-hrundel
+
+## Как установить
+
+```bash
+npm install --save-dev stylelint@7.3.1 stylelint-config-hrundel
+```
+
+## Как использовать
+
+Для кода, исполняемого в Node.js в `package.json` добавляем:
+
+```json
+{
+  "extends": "stylelint-config-standard"
+}
+
+```
+
---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+	"rules": {
+		"color-hex-case": "lower",
+		"color-hex-length": "short",
+		"color-no-invalid-hex": true,
+		"string-quotes": "single",
+
+		"number-leading-zero": "never",
+		"number-max-precision": 2,
+		"number-no-trailing-zeros": true,
+		"length-zero-no-unit": true,
+
+		"declaration-block-semicolon-newline-after": "always",
+
+		"block-closing-brace-newline-after": "always",
+		"block-closing-brace-newline-before": "always",
+		"block-opening-brace-newline-after": "always",
+		"block-opening-brace-newline-before": "always",
+		"block-no-empty": true,
+
+		"selector-combinator-space-after": "always",
+		"selector-combinator-space-before": "always",
+		"selector-list-comma-newline-after": "always",
+
+		"declaration-block-no-duplicate-properties": true,
+		"declaration-block-trailing-semicolon": "always",
+
+		"indentation": 4,
+		"no-missing-end-of-source-newline": true,
+		"max-empty-lines": 1
+	}
+};
